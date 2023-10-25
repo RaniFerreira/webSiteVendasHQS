0be9aec77ac500bcb8e925e9e9b68c2e9c5b6e71
RaniFerreira <129872650+RaniFerreira@users.noreply.github.com>
2023-10-25 08:58:13 -0300

    primeiro commit

---
+++ js/dados.js
@@ -0,0 +1,503 @@
+var MENU = {
+    "TMonica": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/REvistaTurmaMonnica/01225555.jpg",
+            "name": "Mônica 100",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 7.40
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/REvistaTurmaMonnica/monica3.jpg",
+            "name": "Limoeiro",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 9
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/REvistaTurmaMonnica/monica8.jpg",
+            "name": "Comer sem disperdiçar",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 10.60
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "./img/REvistaTurmaMonnica/monica6.jpg",
+            "name": "Alimentos Saudaveis",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 25.00
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+            "img": "./img/REvistaTurmaMonnica/monica33.jpg",
+            "name": "Nutrição",
+            "dsc": "Whole Texas Smoked Brisket",
+            "price": 18.99
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "./img/REvistaTurmaMonnica/monica21.jpg",
+            "name": "Cascao",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 13
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "./img/REvistaTurmaMonnica/monica66.jpg",
+            "name": "Cebolinha",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 23.66
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "./img/REvistaTurmaMonnica/monica22.jpg",
+            "name": "Magali",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 15
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img":"./img/REvistaTurmaMonnica/monica200.jpg",
+            "name": "Numero um",
+            "dsc": "The Big Ugly BBQ Dinner for 6",
+            "price": 23.99
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+            "img": "./img/REvistaTurmaMonnica/monica20.jpg",
+            "name": "Maior vilão",
+            "dsc": "Mighty Quinn's BBQ Sampler Pack",
+            "price": 16.99
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+            "img": "./img/REvistaTurmaMonnica/monica220.jpg",
+            "name": "Todos a bordo",
+            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
+            "price": 33.20
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+            "img": "./img/REvistaTurmaMonnica/monica230.jpg",
+            "name": "Amigo diferente",
+            "dsc": "Best of Texas BBQ Combo - Serves 14",
+            "price": 26
+        }
+    ],
+    "monicaJovem": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/monicajovem/jovem1.jpg",
+            "name": "Parque assombrado",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 19.40
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/monicajovem/jovem3.jpg",
+            "name": "Nunca Mais",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 23.30
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/monicajovem/jovem2.jpg",
+            "name": "A torre inversa",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 10.60
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img":  "./img/monicajovem/jovem4.jpg",
+            "name": "Eles voltaram",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 25.00
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+            "img":  "./img/monicajovem/jovem5.jpg",
+            "name": "Apanhador de pesadelos",
+            "dsc": "Whole Texas Smoked Brisket",
+            "price": 18.99
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img":  "./img/monicajovem/jovem6.jpg",
+            "name": "Eterno despertar",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 21.90
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img":  "./img/monicajovem/jovem78.jpg",
+            "name": "Perigo eletronico",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 12.89
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img":  "./img/monicajovem/jovem10.jpg",
+            "name": "MPortal das trevas",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 15
+        },
+       
+    ],
+    "marvel": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "./img/revistasMarvel/marvel1.jpg",
+            "name": "Heróis Marvel",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 21.70
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "./img/revistasMarvel/marvel2.jpg",
+            "name": "Guerra civil",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "./img/revistasMarvel/marvel6.jpg",
+            "name": "Homem-Aranha",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 40.90
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "./img/revistasMarvel/marvel8.jpg",
+            "name": "Vingadores",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 12.99
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "./img/revistasMarvel/marvel9.jpg",
+            "name": "Hulk",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 12
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "./img/revistasMarvel/marvel10.jpg",
+            "name": "Vingadores para sempre",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 20.99
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img": "./img/revistasMarvel/marvel11.jpg",
+            "name": "Ms. Marvel",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 25
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/revistasMarvel/marvel12.jpg",
+            "name": "X-men",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 45
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img":"./img/revistasMarvel/marvel13.jpg",
+            "name": "Ultron",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 50
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img":"./img/revistasMarvel/marvel14.jpg",
+            "name": "Wolverine",
+            "dsc": "Goldbelly Burger Blend - 4 lbs.",
+            "price": 12
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img": "./img/revistasMarvel/marvel16.jpg",
+            "name": "Justiceiro",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 22.99
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img": "./img/revistasMarvel/marvel17.jpg",
+            "name": "Vision & ScarletWich",
+            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
+            "price": 56.80
+        }
+    ],
+    "Dragon": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/dragon/b1.jpg",
+            "name": "Dragon Ball Super 13",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 39.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/dragon/b2.jpg",
+            "name": "Dragon Ball Super 19",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 39.95
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/dragon/b3.jpg",
+            "name": "Dragon Ball Super 16",
+            "dsc": "Original Cannoli Pie",
+            "price": 69
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "./img/dragon/b4.jpg",
+            "name": "Dragon Ball Super 15",
+            "dsc": "Sea-Salted Caramel Swirl Cheesecake",
+            "price": 40
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "./img/dragon/b6.jpg",
+            "name": "Dragon Ball Super 11",
+            "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
+            "price": 89
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "./img/dragon/b7.jpg",
+            "name": "Dragon Ball Super 14",
+            "dsc": "Best Seller Cupcake Dozen",
+            "price": 89
+        },
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+            "img": "./img/dragon/b8.jpg",
+            "name": "Dragon Ball Super 13",
+            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
+            "price": 69
+        },
+        {
+            "id": "17481-jewish-dessert-3-pack",
+            "img": "./img/dragon/b9.jpg",
+            "name": "Dragon Ball Super 17",
+            "dsc": "Jewish Classics Dessert Pack",
+            "price": 89.95
+        },
+       
+    ],
+    "naruto": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/revistasNaruto/N1.jpg",
+            "name": "Kishimoto 33",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 5.90
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/revistasNaruto/n2.jpg",
+            "name": "Kishimoto 12",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 6.9
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/revistasNaruto/n3.jpg",
+            "name": "Naruto",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 39.30
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/revistasNaruto/n4.jpg",
+            "name": "Naruto v6",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 39
+        },
+        {
+            "id": "margarita-mix",
+            "img": "./img/revistasNaruto/n5.jpg",
+            "name": "Naruto v70",
+            "dsc": "Margarita Mix",
+            "price": 59
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/revistasNaruto/n6.jpg",
+            "name": "Naruto v46",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 59
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/revistasNaruto/n8.jpg",
+            "name": "Naruto v71",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 10
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/revistasNaruto/n7.jpg",
+            "name": "Naruto v62",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 89
+        },
+       
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/bebidas/nola-cold-brew-concentrate-bag-in-box.3df6fde8bd83f29235565984ae8ed22b.jpg",
+            "name": "Grady's Cold Brew",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 40
+        }
+    ],
+    "dc": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/revistasdc/Arlequina.jpg",
+            "name": "Arlequina",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 12.80
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/revistasdc/DCComics_Divulgacao.jpg",
+            "name": "Liga-da-Justiça",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 32.88
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/revistasdc/trinity.jpg",
+            "name": "Trinity",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 22.63
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/revistasdc/dc.jpeg",
+            "name": "DC 2000",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 16
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/revistasdc/batman.jpg",
+            "name": "Batman",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 15.35
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/revistasdc/flash.jpg",
+            "name": "flash",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 20
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/revistasdc/shazam.jpg",
+            "name": "Shazam",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 10.90
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "./img/revistasdc/lexLuthor.jpg",
+            "name": "Lex Luthor",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 150.80
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+            "img": "./img/revistasdc/coringa.jpg",
+            "name": "Coringa",
+            "dsc": "",
+            "price": 40.99
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/revistasdc/Mulher-Maravilha.jpg",
+            "name": "Mulher Maravilha",
+            "dsc": "4 Lou Malnati's Deep Dish Pizzas",
+            "price": 15.99
+        },
+        {
+            "id": "tonys-custom-pizza-3-pack",
+            "img": "./img/revistasdc/laternaverde.jpg",
+            "name": "Lanterna Verde",
+            "dsc": "Choose Your Own Pizza - 3 Pack",
+            "price": 9.40
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+            "img": "./img/revistasdc/Genesis.jpg",
+            "name": "Gênesis",
+            "dsc": "Plain Thin Crust Pizza - 4 Pack",
+            "price": 99.99
+        }
+    ],
+    "piece": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img":  "./img/onepiece/one1.jpg",
+            "name": "Red",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 23.90
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img":"./img/onepiece/one2.jpg",
+            "name": "Oda 75",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 55.99
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/onepiece/one3.jpg",
+            "name": "Oda 56",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 17.66
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/onepiece/one4.jpg",
+            "name": "Oda 78",
+            "dsc": "Bone-in Rib Steak",
+            "price": 58.33
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/onepiece/one5.jpg",
+            "name": "Oda 100",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 119.30
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/onepiece/one6.jpg",
+            "name": "Oda 86",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 25.95
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img":"./img/onepiece/one7.jpg",
+            "name": "Oda 17",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 95.98
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/onepiece/one8.jpg",
+            "name": "Oda 19",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 96.7
+        },
+        
+    ]
+}
